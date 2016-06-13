992f54a161a4bc280e7a39290f036ae2a1f6ceb9
Eric Snell <eric_snell21@yahoo.com>
2016-06-13 17:32:53 -0400

    finished hadouken

--- js/app.js
+++ js/app.js
@@ -6,5 +6,28 @@ $(document).ready(function() {
   .mouseleave(function() {
   	$('.ryu-ready').hide();
   	$('.ryu-still').show();
+  })
+  .mousedown(function() {
+  	playHadouken();
+  	$(".ryu-ready").hide();
+  	$(".ryu-throwing").show();
+  	$(".hadouken").finish().show().animate(
+  		{"left": "1020px"},
+  		500,
+  		function() {
+  			$(this).hide();
+  			$(this).css("left", "620px");
+  		}
+  	);
+  })
+  .mouseup(function() {
+  	$(".ryu-throwing").hide();
+  	$(".ryu-ready").show();
   });
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
